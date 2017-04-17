4454931318e39c13d65f94416ff90c1bac82a3ce
thebernardlim <ber9000@gmail.com>
2017-04-17 13:50:03 +0800

    8kyu-InvertValues - new

---
+++ 8kyu-InvertValues/8kyu-InvertValues/ArraysInversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8kyu_InvertValues
+{
+
+    public static class ArraysInversion
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static int[] InvertValues(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] *= -1;
+            }
+
+            return input;
+        }
+    }
+}
