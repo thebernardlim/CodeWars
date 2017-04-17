8a367f9d2c231e6817feacc1c1308e254d1a5fa7
thebernardlim <ber9000@gmail.com>
2017-04-17 22:48:49 +0800

    7kyu-ExesandOhs

---
+++ 7kyu-ExesandOhs/7kyu-ExesandOhs/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7kyu_ExesandOhs
+{
+    public static class Kata
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static bool XO(string input)
+        {
+            char[] chars = input.ToLower().ToCharArray();
+            int x = 0;
+            int o = 0;
+            foreach (char c in chars)
+            {
+                if (c == 'o') o++;
+                if (c == 'x') x++;
+            }
+
+            if (x == o) return true;
+            else return false;
+
+        }
+    }
+
+
+}
---
+++ 7kyu-ExesandOhs/TestProject/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using _7kyu_ExesandOhs;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(true, Kata.XO("xo"));
+            Assert.AreEqual(true, Kata.XO("xxOo"));
+            Assert.AreEqual(false, Kata.XO("xxxm"));
+            Assert.AreEqual(false, Kata.XO("Oo"));
+            Assert.AreEqual(false, Kata.XO("ooom"));
+        }
+    }
+}
